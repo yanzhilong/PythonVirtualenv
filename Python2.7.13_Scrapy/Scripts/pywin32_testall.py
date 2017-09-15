db6bc2d3e03328f92e9e66c6776a873fa21a65e7
yanzl <51877421@qq.com>
2017-09-15 19:40:44 +0800

    更新Python2.7.13_Scrapy项目：添加数据库，添加win32

---
+++ Python2.7.13_Scrapy/Scripts/pywin32_testall.py
@@ -0,0 +1,4 @@
+#!D:\Projects\PythonVirtualenv\Python2.7.13_Scrapy\Scripts\python.exe
+# EASY-INSTALL-SCRIPT: 'pywin32==219',u'pywin32_testall.py'
+__requires__ = 'pywin32==219'
+__import__('pkg_resources').run_script('pywin32==219', u'pywin32_testall.py')
